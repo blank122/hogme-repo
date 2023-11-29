a48484b7002dc4e87b0835822433461ea353e893
Joshua De Vera <deverajoshua02@gmail.com>
2023-11-29 19:51:38 +0800

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hogme_user_prod/firebase_options.dart';
+import 'package:hogme_user_prod/services/authentication_services/auth_changes.dart';
 import 'package:hogme_user_prod/services/authentication_services/authentication_services.dart';
 import 'package:provider/provider.dart';
 
